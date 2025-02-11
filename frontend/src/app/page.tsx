e676a869af7e98f82b6042ff8bcdbca0d59c7c0c
heitzlki <kirill.heitzler@gmail.com>
2025-02-11 18:08:38 +0100

    feat: landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,76 @@
-import Image from "next/image";
+'use client';
+
+import { FlickeringGrid } from '@/components/magicui/flickering-grid';
+import { AnimatedSubscribeButton } from '@/components/magicui/animated-subscribe-button';
+import { CheckIcon, ChevronRightIcon } from 'lucide-react';
+
+import { RainbowButton } from '@/components/magicui/rainbow-button';
+import { RetroGrid } from '@/components/magicui/retro-grid';
+import Survey from '@/app/survey';
+import { TypingAnimation } from '@/components/magicui/typing-animation';
+import { Input } from '@/components/ui/input';
+import { X } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = useRouter();
+  const [start, setStart] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  return (
+    <div
+      className='relative flex h-lvh w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background'
+      suppressHydrationWarning>
+      <FlickeringGrid
+        className='absolute inset-0 z-0 size-full'
+        squareSize={6}
+        gridGap={8}
+        color='#6B7280'
+        maxOpacity={0.5}
+        flickerChance={0.1}
+      />
+      <TypingAnimation className='z-10 md:text-6xl font-bold text-2xl'>
+        Start journey!
+      </TypingAnimation>
+      {start ? (
+        <div className='m-10 flex flex-row items-center justify-center'>
+          <div className='mx-4 relative flex h-10 w-fit items-center justify-center overflow-hidden rounded-lg bg-primary  text-primary-foreground'>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              playholder='Email'
+              className='w-full p-2 text-black placeholder-slate-800 bg-transparent focus:outline-none'
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+
+          <AnimatedSubscribeButton subscribeStatus={false} className='w-32'>
+            <span className='group inline-flex items-center'>
+              Follow
+              <ChevronRightIcon className='ml-1 size-4 transition-transform duration-300 group-hover:translate-x-1' />
+            </span>
+            {false ? (
+              <span className='group inline-flex items-center'>
+                <CheckIcon className='mr-2 size-4' />
+                Subscribed
+              </span>
+            ) : (
+              <span className='group inline-flex items-center text-red-500'>
+                Invalid
+                <X className='mr-2 size-4 translate-x-2' strokeWidth={3} />
+              </span>
+            )}
+          </AnimatedSubscribeButton>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ) : (
+        <RainbowButton
+          className='m-10'
+          onClick={() => {
+            setStart(true);
+          }}>
+          Join now!
+        </RainbowButton>
+      )}
     </div>
   );
 }
